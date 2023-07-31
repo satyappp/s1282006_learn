f7bb41abb6e6bb69a9cdd4f80c6802b7d7887f0a
satyappp <s1282006@u-aizu.ac.jp>
2023-07-31 20:29:45 +0900

    Completetd ex14

---
+++ setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+    
+setuptools.setup(
+    name='pami',
+    version='2023.07.31',
+    author='Satyabrata Pahari',
+    author_email='s1282006@u-aizu.ac.jp',
+    description='This software is being developed at the University of Aizu, Aizu-Wakamatsu, Fukushima, Japan',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages = setuptools.find_packages(),
+    url='https://github.com/satyappp/s1282006_learn',
+    license='GPLv3',
+    install_requires=[ 
+        'pandas',
+        'plotly',
+        'matplotlib',
+        'pami',
+        'numpy',
+    ],
+    extras_require={
+        'gpu': ['cupy', 'pycuda'],
+        'spark': ['pyspark'],
+        'dev': ['twine', 'setuptools', 'build'],
+        'all': ['cupy', 'pycuda', 'pyspark', 'twine', 'setuptools', 'build']},
+    classifiers=[
+        'Development Status :: 4 - Beta', # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: 0S Independent',
+    ],
+    python_requires='>=3.5',
+)
